3b408d67bdf707672d56b2b0cd0868ac90bffa2f
tusfendi <tusfendi@gmail.com>
2023-04-15 14:32:44 +0700

    add readme and db

---
+++ database/festival_movie_db.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE `festival_movie_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+-- festival_movie_db.actor definition
+
+CREATE TABLE `actor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.genre definition
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genre` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.movie definition
+
+CREATE TABLE `movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `movies_file_id` int(11) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` text,
+  `duration` time DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.movies_actor definition
+
+CREATE TABLE `movies_actor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `actor_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.movies_file definition
+
+CREATE TABLE `movies_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_path` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.movies_genre definition
+
+CREATE TABLE `movies_genre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genre_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.movies_like definition
+
+CREATE TABLE `movies_like` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `movie_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.`user` definition
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) DEFAULT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` enum('ADMIN','USER') DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- festival_movie_db.user_token definition
+
+CREATE TABLE `user_token` (
+  `guid` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Festival Movie Backend Service
+
+This backend service is for Festival movie apps
+
+## Contact
+| Name                   | Email                           |
+| :--------------------: |:-------------------------------:|
+| Tusfendi               | tusfendi@gmail.com              |
+
+## Onboarding and Development Guide
+### Prerequisite
+- Git (See [Git Installation](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git))
+- Go 1.20 or later (See [Golang Installation](https://golang.org/doc/install))
+- MySQL (See [MySQL Installation](https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/))
+
+
+### Installation
+- Clone this repo
+
+    ```sh
+        git clone https://github.com/tusfendi/festival-movie-be.git
+    ```
+
+- Copy `.env.example` to `.env`
+
+    ```sh
+        cp .env.example .env
+    ```
+- Setup local database, you can import database from database/festival_movie_db.sql
+
+- Start service API
+    ```sh
+        go run cmd/api/main.go
+    ```
