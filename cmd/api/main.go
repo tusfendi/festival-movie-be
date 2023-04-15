b92d5fef7a6f24bb02c9fbe43ebb6ad7418c053f
tusfendi <tusfendi@gmail.com>
2023-04-15 19:16:26 +0700

    fix: update route path

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,6 +30,7 @@ func main() {
 	r.Use(middleware.CORSMiddleware())
 
 	r.GET("/foo", func(c *gin.Context) {
+		// strings.Split(c.Request.URL.Path, "/")
 		fmt.Println("The URL: ", c.Request.Host+c.Request.URL.Path)
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{"response": c.Request.Host})
 	})
@@ -37,25 +38,47 @@ func main() {
 	// repository
 	actorRepository := repository.NewActorRepository(mysqlConn)
 	genreRepository := repository.NewGenreRepository(mysqlConn)
+	userHitoryRepository := repository.NewUserHistoryRepository(mysqlConn)
+	movieRepository := repository.NewMovieRepository(mysqlConn)
+	movieGenreRepository := repository.NewMoviesGenreRepository(mysqlConn)
+	movieActorRepository := repository.NewMoviesActorRepository(mysqlConn)
 
 	// usecase
 	actorUsecase := usecase.NewActorUsecase(actorRepository)
 	genreUsecase := usecase.NewGenreUsecase(genreRepository)
+	movieGenreUsecase := usecase.NewMoviesGenreUsecase(movieGenreRepository)
+	movieUsecase := usecase.NewMovieUsecase(movieRepository, movieGenreRepository, movieActorRepository, userHitoryRepository)
+
+	// TBU untuk middleware auth user
+
+	// admin use : /_internal
+	admin := r.Group("/_internal")
+	// add middleware to check role is admin
 
 	// Handler
 	// actor
-	r.GET("/artists", actorUsecase.Get)
-	r.POST("/artists", actorUsecase.CreateActor)
-	r.GET("/artists/:id", actorUsecase.GetDetail)
-	r.PATCH("/artists/:id", actorUsecase.UpdateActor)
-	r.DELETE("/artists/:id", actorUsecase.DeleteActor)
+	admin.GET("/artists", actorUsecase.Get)
+	admin.POST("/artists", actorUsecase.CreateActor)
+	admin.GET("/artists/:id", actorUsecase.GetDetail)
+	admin.PATCH("/artists/:id", actorUsecase.UpdateActor)
+	admin.DELETE("/artists/:id", actorUsecase.DeleteActor)
 
 	// genre
-	r.GET("/genres", genreUsecase.Get)
-	r.POST("/genres", genreUsecase.CreateGenre)
-	r.GET("/genres/:id", genreUsecase.GetDetail)
-	r.PATCH("/genres/:id", genreUsecase.UpdateGenre)
-	r.DELETE("/genres/:id", genreUsecase.DeleteGenre)
+	admin.GET("/genres", genreUsecase.Get)
+	admin.POST("/genres", genreUsecase.CreateGenre)
+	admin.GET("/genres/:id", genreUsecase.GetDetail)
+	admin.PATCH("/genres/:id", genreUsecase.UpdateGenre)
+	admin.DELETE("/genres/:id", genreUsecase.DeleteGenre)
+
+	r.GET("/movies", movieUsecase.Get)
+	r.GET("/movies/:id", movieUsecase.GetDetail)
+	admin.POST("/movies", movieUsecase.CreateMovie)
+	admin.PATCH("/movies/:id", movieUsecase.UpdateMovie)
+	admin.DELETE("/movies/:id", movieUsecase.DeleteMovie)
+
+	admin.POST("/movies-genre", movieGenreUsecase.CreateMoviesGenre)
+	admin.DELETE("/movies-genre/:id", movieGenreUsecase.Delete)
+	admin.GET("/movies-genre/:id", movieGenreUsecase.GetMoviesGenresByMovieID)
 
 	r.Run(":" + fmt.Sprint(cfg.ApiPort))
 }
