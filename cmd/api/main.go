c9f22c8ebeecf3498df5fb019380924e1cf38e74
tusfendi <tusfendi@gmail.com>
2023-04-15 15:23:14 +0700

    config: routing api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
+	"github.com/tusfendi/festival-movie-be/cmd/api/middleware"
+	"github.com/tusfendi/festival-movie-be/config"
+)
+
+func init() {
+	_ = gotenv.Load()
+}
+
+func main() {
+	cfg := config.NewConfig()
+	_, err := config.NewMysql(cfg.AppEnv, &cfg.MysqlOption)
+	if err != nil {
+		log.Fatal(err)
+		println("error mysql")
+	}
+
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+	r.Use(middleware.CORSMiddleware())
+
+	r.GET("/foo", func(c *gin.Context) {
+		fmt.Println("The URL: ", c.Request.Host+c.Request.URL.Path)
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"response": c.Request.Host})
+	})
+
+	r.Run(":" + fmt.Sprint(cfg.ApiPort))
+}
---
+++ cmd/api/middleware/middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tusfendi/festival-movie-be/config"
+)
+
+func JwtAuthMiddleware(key string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := config.TokenValid(key, c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"response": "gagal", "error": "Anda tidak ada Akses", "error_detail": err.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
+
+// cors middleware
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-user-agent, client-key")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PACTH")
+
+		if c.Request.Method == "OPTIONS" || c.Request.Method == "PUT" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
